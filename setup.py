a70345f55d4affea5265630d126fa464b5afae5c
automorphis <mlanetheta@gmail.com>
2023-12-05 12:29:00 -0500

    updates

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name = 'mplanetypes',
     version = 0.1,
     description = "A few utility type-checking functions that I use a lot.",
+    long_description = "A few utility type-checking functions that I use a lot.",
+    long_description_content_type = "text/plain",
     author = "Michael P. Lane",
     author_email = "mlanetheta@gmail.com",
     url = "https://github.com/automorphis/mplanetypes",
